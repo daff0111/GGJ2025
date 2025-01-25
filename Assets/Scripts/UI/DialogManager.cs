a608679501a56bde83c9b32dcf042f696f185ffb
Domenico Favaro <daff0111.df@gmail.com>
2025-01-25 21:45:56 +0100

    Agregados Bugles y primer dialogo

--- Assets/Scripts/UI/DialogManager.cs
+++ Assets/Scripts/UI/DialogManager.cs
@@ -9,6 +9,7 @@ public class DialogManager : MonoBehaviour
     [SerializeField] GameObject dialogBox;
     [SerializeField] Text dialogText;
     [SerializeField] int lettersPerSecond;
+    public AudioClip confirmSound;
     private bool isActive = false;
     private bool canAdvance = false;
     private int lineIndex = 0;
@@ -17,6 +18,8 @@ public class DialogManager : MonoBehaviour
     public event Action OnDialogStarted;
     public event Action OnDialogEnded;
 
+    private AudioSource audioSource;
+
     public static DialogManager Instance { get; private set; }
 
     private void Awake()
@@ -24,6 +27,7 @@ public class DialogManager : MonoBehaviour
         Instance = this;
         isActive = false;
         dialogBox.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
     }
     public void HandleUpdate()
     {
@@ -34,6 +38,7 @@ public class DialogManager : MonoBehaviour
                 lineIndex++;
                 if (lineIndex < activeDialog.Lines.Count)
                 {
+                    PlayConfirmSound();
                     StartCoroutine(TypeDialog(activeDialog.Lines[lineIndex]));
                 }
                 else
@@ -56,7 +61,8 @@ public class DialogManager : MonoBehaviour
         activeDialog = dialog;
         dialogBox.SetActive(true);
         OnDialogStarted.Invoke();
-        
+
+        PlayConfirmSound();
         StartCoroutine(TypeDialog(dialog.Lines[lineIndex]));
     }
 
@@ -66,6 +72,15 @@ public class DialogManager : MonoBehaviour
         dialogBox.SetActive(false);
         
         isActive = false;
+    }
+
+    public void PlayConfirmSound()
+    {
+        if (audioSource != null)
+        {
+            audioSource.clip = confirmSound;
+            audioSource.Play();
+        }
     }
 
     public IEnumerator TypeDialog(string line)
